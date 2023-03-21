bb68caec91467a273f6989169a101c5a1b86e49d
artemrbb <artemrbb@gmail.com>
2023-03-21 17:57:14 +0400

    Second commit

---
+++ crm/crm/Models/LegalPersonModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crm.Models
+{
+    class LegalPersonModel
+    {
+        #region Constructor
+
+        public LegalPersonModel(string nameCompany, string inn, string ogrn, string contry, string city, string address, string email, string phoneNumber)
+        {
+            _nameCompany = nameCompany;
+            _inn = inn;
+            _ogrn = ogrn;
+            _contry = contry;
+            _city = city;
+            _address = address;
+            _email = email;
+            _phoneNumber = phoneNumber;
+        }
+
+        #endregion
+
+        #region Fields
+
+
+        private string _nameCompany;
+        private string _inn;
+        private string _ogrn;
+        private string _contry;
+        private string _city;
+        private string _address;
+        private string _email;
+        private string _phoneNumber;
+
+        #endregion
+
+        #region Properties
+
+        public string NameCompany { get => _nameCompany; set => _nameCompany = value; }
+        public string Inn { get => _inn; set => _inn = value; }
+        public string Ogrn { get => _ogrn; set => _ogrn = value; }
+        public string Contry { get => _contry; set => _contry = value; }
+        public string City { get => _city; set => _city = value; }
+        public string Address { get => _address; set => _address = value; }
+        public string Email { get => _email; set => _email = value; }
+        public string PhoneNumber { get => _phoneNumber; set => _phoneNumber = value; }
+
+        #endregion
+    }
+}
---
+++ crm/crm/Program/ProgramControls.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UltimateCore.AppManagement;
+using UltimateCore.LRI;
+
+namespace crm.Program
+{
+    sealed class ProgramControls : Singleton<ProgramControls>
+    {
+        #region Constructor
+
+        public ProgramControls()
+        {
+            _appFactory = AppFactory.GetInstance();
+        }
+
+        #endregion
+
+        #region Fields
+
+        private readonly AppFactory _appFactory;
+
+        #endregion
+
+        #region Methods
+
+        public Result<bool> Initialize()
+        {
+            return new Result<bool>(() =>
+            {
+
+                return true;
+            });
+        }
+
+        public Result<string> GetInfofromDB(string command)
+        {
+            return new Result<string>(() =>
+            {
+
+                if (string.IsNullOrEmpty(command))
+                    return "Команда пуста. Воспользуйтесь командой /help";
+                else if (command == "/inoneyear") // 1.	Вывести сумму всех заключенных договоров за текущий год.
+                {
+
+                }
+                else if (command == "/fromrus") // 2.	Вывести сумму заключенных договоров по каждому контрагенту из России.
+                {
+
+                }
+                else if (command == "/emails")// вывести email физ лиц заключивших договор за последние 30 дней на сумму больше 40000 
+                {
+
+                }
+                else if (command == "/changestatus") // 4.	Изменить статус договора на "Расторгнут" для физических лиц, у которых есть действующий договор, и возраст которых старше 60 лет включительно.
+                {
+
+                }
+                else if (command == "/createreport") // 5.	Создать отчет (текстовый файл, например, в формате xml, xlsx, json) содержащий ФИО, e-mail, моб. телефон, дату рождения физ. лиц, у которых есть действующие договора по компаниям, расположенных в городе Москва. 
+                {
+
+                }
+                else if (command == "/help")
+                {
+                    return $"/inoneyear - Вывести сумму всех заключенных договоров за текущий год\n" +
+                    $"/fromrus - Вывести сумму заключенных договоров по каждому контрагенту из России\n" +
+                    $"/emails - Вывести email физ лиц заключивших договор за последние 30 дней на сумму больше 40000\n" +
+                    $"/changestatus - Изменить статус договора на \"Расторгнут\" для физических лиц, у которых есть действующий договор, и возраст которых старше 60 лет включительно.\n" +
+                    $"/createreport - Создать отчет (текстовый файл, например, в формате xml, xlsx, json) содержащий ФИО, e-mail, моб. телефон, дату рождения физ. лиц, у которых есть действующие договора по компаниям, расположенных в городе Москва";
+                }
+                else
+                    return "Команда не найдена";
+
+                return null;
+            });
+        }
+
+        #endregion
+    }
+}
